d5fae4ce81bfb1b3b118d5e7608367603ddc05c5
Yiannis Pipertzis <yiannn@gmail.com>
2019-07-14 18:00:43 +1000

    Updates

---
+++ precache-manifest.b3600b9bf720847085049b822c37aff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cfd13cf24321c805fae89731a9a0e2",
+    "url": "/shuffler/index.html"
+  },
+  {
+    "revision": "27c05935b5f416797d99",
+    "url": "/shuffler/static/css/main.0af45f22.chunk.css"
+  },
+  {
+    "revision": "2116849138348b18d6c9",
+    "url": "/shuffler/static/js/2.598a9db1.chunk.js"
+  },
+  {
+    "revision": "27c05935b5f416797d99",
+    "url": "/shuffler/static/js/main.2973c393.chunk.js"
+  },
+  {
+    "revision": "a04bb4216a883c9d5227",
+    "url": "/shuffler/static/js/runtime~main.eec3e2ea.js"
+  },
+  {
+    "revision": "a6fedafc1d02050483786f9e580e4fc1",
+    "url": "/shuffler/static/media/DALEK.a6fedafc.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shuffler/static/media/logo.5d5d9eef.svg"
+  }
+]);
